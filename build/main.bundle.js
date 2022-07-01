693f4b93cf62ccfb6d0fa2f1ea993973991c9a76
GitPaulo <paulo.santos98@gmail.com>
2022-07-02 00:37:46 +0100

    zooming added!

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -16493,31 +16493,59 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
 const pdfjsLib = __webpack_require__(1);
-const loadingTask = pdfjsLib.getDocument('./resources/paulo_resume.pdf');
+const loadingTask = pdfjsLib.getDocument("./resources/paulo_resume.pdf");
 
-loadingTask.promise.then((pdf) => {
-    pdf.getPage(1).then((page) => {
-        page.getAnnotations().then(console.log)
-        let scale = 1.5;
-        let viewport = page.getViewport({
-            scale: scale,
-        });
+let scale = 1.25;
+let pdf = null;
 
-        let canvas = document.getElementById('resume_canvas');
-        let context = canvas.getContext('2d');
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
+loadingTask.promise.then((_pdf) => {
+  pdf = _pdf;
+  pdf.getPage(1).then((page) => {
+    renderDocument(page, scale);
 
-        let renderContext = {
-            canvasContext: context,
-            viewport: viewport
-        };
+    // for new browsers only
+    let lastWidth = window.innerWidth;
+    let lastHeight = window.innerHeight;
+    window.addEventListener("resize", (event) => {
+      let newWidth = window.innerWidth;
+      let newHeight = window.innerHeight;
 
-        page.render(renderContext);
+      if (newWidth < lastWidth || newHeight < lastHeight) {
+        zoomIn(0.25);
+      } else {
+        zoomOut(0.25);
+      }
 
-        console.log("Completed!");
+      lastWidth = newWidth;
+      lastHeight = newHeight;
     });
+  });
 });
+
+function zoomIn(cscale) {
+  pdf.getPage(1).then((page) => renderDocument(page, (scale += cscale)));
+}
+
+function zoomOut(cscale) {
+  pdf.getPage(1).then((page) => renderDocument(page, (scale -= cscale)));
+}
+
+function renderDocument(page, scale) {
+  let viewport = page.getViewport({ scale: scale });
+  let canvas = document.getElementById("resume_canvas");
+  let context = canvas.getContext("2d");
+  canvas.height = viewport.height;
+  canvas.width = viewport.width;
+
+  page.render({
+    canvasContext: context,
+    viewport,
+  });
+}
+
+// for webpack
+window.zoomIn = zoomIn;
+window.zoomOut = zoomOut;
 
 })();
 
